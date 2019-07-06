73030467a36a64ae8a1de34295359c9068b45b0e
ctan01 <48540650+ctan01@users.noreply.github.com>
2019-07-06 15:02:19 -0400

    Update btNode.h

--- btNode.h
+++ btNode.h
@@ -1,1 +1,24 @@
+#ifndef __BTNODE_H__
+#define __BTNODE_H__
+#include <iostream>
+#include <cstdlib>
+using namespace std;
 
+template <class Item>
+class btNode{
+  public:
+    btNode(){
+      const Item& data = Item( ),
+      left = NULL;
+      right = NULL;
+      parent = NULL;
+    }
+  private:
+    Item data;
+    btNode *left;
+    btNode *right;
+    btNode *parent;
+};
+
+#include "btNode.cpp"
+#endif
