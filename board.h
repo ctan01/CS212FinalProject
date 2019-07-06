482fd85f2db5977fc9a21ec89883c49e172d3b6a
CChariot <48741996+CChariot@users.noreply.github.com>
2019-07-06 14:52:46 -0400

    Update board.h

--- board.h
+++ board.h
@@ -1,1 +1,29 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class board{
 
+public:
+	board(){
+
+	board = {{0,1,0,1,0,1,0,1},
+				   {1,0,1,0,1,0,1,0},
+				   {0,1,0,1,0,1,0,1},
+				   {0,0,0,0,0,0,0,0},
+				   {0,0,0,0,0,0,0,0},
+				   {1,0,1,0,1,0,1,0},
+				   {0,1,0,1,0,1,0,1},
+				   {1,0,1,0,1,0,1,0}};
+
+
+}
+
+private:
+	static int board[8][8];
+
+
+
+};
+#endif
