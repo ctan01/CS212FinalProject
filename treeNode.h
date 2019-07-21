4ee596556260211e733ab8368cc46ee051db1041
ctan01 <48540650+ctan01@users.noreply.github.com>
2019-07-21 11:04:00 -0400

    Update treeNode.h

--- treeNode.h
+++ treeNode.h
@@ -284,9 +284,10 @@ for(int i = 0; i < 8; i++){
    {
      genChild(dfsStack.top()); //generate all the children top the top item on the stack.
       
-        
-           dfsStack.push(dfsStack.top()* children [0]); // push the first generated children of inParent into the stack
-      
+        for(int i=0;i<dfsStack.top().numChildren;i++)
+          {
+           dfsStack.push(dfsStack.top()* children [i]); // push all the generated children of inParent into the stack
+          }
                
       if(dfsStack.top().data.winningState())
       {
@@ -301,11 +302,26 @@ for(int i = 0; i < 8; i++){
 treeNode* greedyAlg(treeNode* inParent){
 	treeNode *curr; 
 	curr = inParent;
+	board temp1;
+	int closest1i = 7;
+	int closest2i = 0;
+	board temp2;
 	while(!curr.data.winningState()){//find the node that contains the board that have a piece closest to the enemy's edge.
 		for(int k = 0; k < numChildren; k++){
 			for(int i=0; i< 8;i++){
        				for(int j=0; j<8; j++){
-					
+					boardArray[i][j] == 2;
+					if(i < closest2i){
+						cloest2i = i;
+					}
+				}
+			}
+			for(int i = 7; i < 0; i--){
+				for(int j = 7; j < 0; j--){
+					boardArray[i][j] == 1;
+					if(i > cloest1i){
+						cloest1i = i;
+					}
 				}
 			}
 		}
