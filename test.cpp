62488641fababbbb47385f17c574a9bfa287d9f7
ctan01 <48540650+ctan01@users.noreply.github.com>
2019-07-21 23:57:11 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,12 @@
 #include "board.h"
 #include "treeNode.h"
-#include <iostream>
-using namespace std;
+
 
 int main(){
+  board testBoard;
+  testBoard.printBoard();
+  testBoard.moveLeftDown(2,3);
+  testBoard.printBoard();
+  testBoard.moveLeftDown(0,2);//expected to print "Invalid Move"
   
 }
