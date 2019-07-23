0cc8e290a9f3083191078bca4327366633981fce
CChariot <48741996+CChariot@users.noreply.github.com>
2019-07-23 10:22:18 -0400

    Update treeNode.h

--- treeNode.h
+++ treeNode.h
@@ -3,67 +3,55 @@
 #include <iostream>
 #include <cstdlib>
 #include "board.h"
+#include "board.cpp"
 #include <queue>
 #include <stack>
 
 using namespace std;
 
 class treeNode{
-	
-	global:size_t countTurn=1; // global value to keep track of the turns of the pieces. 
-				   //initialize count to be odd, because black piece moves first in the original state. 
-	
+
+  int countTurn=1; // global value to keep track of the turns of the pieces. 
+           //initialize count to be odd, because black piece moves first in the original state. 
   public:
   
   //constructor
-    treeNode();
-	
-	//we cannot create objects of same class in the constructor by using new keyword
-//so we have to make a function named 'initialize' to initialize the pointers of a treenode
-    void initialize();
-  
-  //copy constructor
-    treeNode(const treeNode &input);
-  
-  void setChild(treeNode *entry, int pos） {     children[pos] = entry;  }
- 
-	
-  treeNode* getChild(int y);
-	
-   treeNode* getAllChild();
-  
-  
- void addChild( treeNode *entry );
-  
-	
-	
-	
-void generateChild(treeNode* inParent);
+  treeNode();
 
+  //copy constructor
+  treeNode( treeNode *input);
+ 
+  
+  treeNode* getChild(int y);
+  
+  treeNode* getAllChild();
+  
+  
+void addChild( treeNode *entry );
          
 void winningMove(treeNode inParent);
-	
-	
-	
+  
+void generateChild(treeNode *inParent);
+  
   treeNode* bfs(treeNode* inParent);
  
               
   treeNode* dfs(treeNode* inParent);
     
 
-	
-	
-	
-	
-	 treeNode* UCS(treeNode *inParent,int x);
+  treeNode* UCS(treeNode *inParent,int x);
            
-   private:
+private:
+
     Board data;
     size_t numChildren;
-    size_t capChildren;
     treeNode *parent;
-    treeNode *children[capChildren];
+    treeNode *children[30];
     
 };
+
+
+  void setChild( treeNode *entry, int pos);
+
 
 #endif
